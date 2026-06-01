8c3c500e9dbad6a0c7d355b2b1aad1c7a2aed310
Serena Di Girolamo <serena.digirolamo@gmail.com>
2026-06-01 15:10:43 +0200

    feat: add function to show selected timezone time

--- javascript/index.js
+++ javascript/index.js
@@ -16,7 +16,22 @@ let sidneyTime = moment().tz("Australia/Sydney");
 sidneyDateElement.innerHTML = sidneyTime.format("MMMM Do YYYY")
 sidneyTimeElement.innerHTML = `${sidneyTime.format("h:mm:ss [<small>]A[</small>]")}`;
 }
+
+function updateCity(event) {
+    let cityTimezone = event.target.value;
+    let cityTime = moment().tz(cityTimezone)
+    let cityName = cityTimezone.replace("_", " "). split("/")[1];
+    let citiesElement = document.querySelector(".cities");
+    citiesElement.innerHTML = 
+        `<div class="city">
+        <div><h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div></div>
+    <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>`;
+}
+
 updateTime()
 setInterval(updateTime, 1000)
 
-
+let citySelectElement = document.querySelector("#cities");
+citySelectElement.addEventListener("change", updateCity);
